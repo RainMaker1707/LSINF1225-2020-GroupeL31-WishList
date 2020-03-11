a1ef6942a2dd2fadf13c2c162f4cd3b232fc3049
Hadrien <hadrienallegaert@icloud.com>
2020-03-11 13:13:09 +0100

    SignIn to SignUp and reverse
    I use a TextView link instead a "back" Button it seems me to be a better way to travel in the differents layouts

--- app/src/main/java/com/coo_project/wishlist/SignIn.java
+++ app/src/main/java/com/coo_project/wishlist/SignIn.java
@@ -2,13 +2,25 @@ package com.coo_project.wishlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignIn extends AppCompatActivity {
-
+    private TextView sign_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        this.sign_up = findViewById(R.id.go_sign_up);
+        this.sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next_layout = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(next_layout);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/coo_project/wishlist/SignUp.java
+++ app/src/main/java/com/coo_project/wishlist/SignUp.java
@@ -2,13 +2,27 @@ package com.coo_project.wishlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
+    private TextView sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        this.sign_in = findViewById(R.id.go_sign_in);
+        this.sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next_layout = new Intent(getApplicationContext(), SignIn.class);
+                startActivity(next_layout);
+                finish();
+            }
+        });
     }
 }
