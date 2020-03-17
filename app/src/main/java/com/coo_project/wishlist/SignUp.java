2f419297b73ed53a89da460b07b4ada6e3683511
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-03-17 17:20:06 +0100

    getText from SignUp editText and conversion to Strings

--- app/src/main/java/com/coo_project/wishlist/SignUp.java
+++ app/src/main/java/com/coo_project/wishlist/SignUp.java
@@ -5,19 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SignUp extends AppCompatActivity {
 
-    private TextView sign_in;
+    private EditText mail_in;
+    private EditText password;
+    private EditText password_conf;
+    private EditText pseudo_in;
+    private Connection connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        this.sign_in = findViewById(R.id.go_sign_in);
-        this.sign_in.setOnClickListener(new View.OnClickListener() {
+        TextView sign_in = findViewById(R.id.go_sign_in);
+        sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent next_layout = new Intent(getApplicationContext(), SignIn.class);
@@ -25,5 +37,39 @@ public class SignUp extends AppCompatActivity {
                 finish();
             }
         });
+
+        Button sign_up = findViewById(R.id.sign_up_button);
+        this.mail_in = findViewById(R.id.mail_in);
+        this.pseudo_in = findViewById(R.id.pseudo_in);
+        this.password = findViewById(R.id.pass_in);
+        this.password_conf = findViewById(R.id.pass_in_conf);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = mail_in.getText().toString();
+                String pseudo = pseudo_in.getText().toString();
+                String pass = password.getText().toString();
+                String pass_conf = password_conf.getText().toString();
+                String password = null;
+                if(pass.equals(pass_conf)){
+                    password = pass;
+                    try{
+                        connection = DriverManager.getConnection("jdbc:sqlite:" + "wishlist_db.db");
+                    }catch(SQLException ex){
+                        connection = null;
+                    }
+                }else{
+                    //TODO error passwords didn't match
+                }
+
+                if (connection != null){
+                    //TODO insert user
+                }else{
+                    //TODO connection error
+                }
+            }
+        });
+
+
     }
 }
